5b2151532286cf3d802bad44470e31fc3e123e1f
zaheersani <sani.zaheer@gmail.com>
2020-01-14 12:31:52 +0500

    Fonts added, Screens Ready, React-Navigation package installed

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+}
 
 export default function App() {
+  const [fontLoadded, setFontLoaded] = useState(false);
+
+  if (!fontLoadded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Text>Screens js files created</Text>
+      <Text>Open-Sans Fonts are Setup!</Text>
+      <Text>React-Navigation Packages Installed</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
