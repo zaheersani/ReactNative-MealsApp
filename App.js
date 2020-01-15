117be9faeebf2ac89ceda4facb272cb7361185cd
zaheersani <sani.zaheer@gmail.com>
2020-01-15 11:31:02 +0500

    Navigation Dependencies Issues Resolved

--- App.js
+++ App.js
@@ -27,10 +27,27 @@ export default function App() {
   return <MealsNavigator />;
 }
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+/*
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
   }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
 });
+
+export default createAppContainer(AppNavigator);
+*/
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,9 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
-import { CategoriesScreen } from "../screens/CategoriesScreen";
-import { CategoryMealsScreen } from "../screens/CategoryMealsScreen";
-import { MealDetailScreen } from "../screens/MealDetailScreen";
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import MealDetailScreen from "../screens/MealDetailScreen";
 
 const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen,
